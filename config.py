6d98928ed6386df7b6692f2a3ce14698abde55f2
Jett-ace <thompcyber@gmail.com>
2026-01-07 01:30:56 +0200

    Update DB_PATH in config.py to casino.db

--- config.py
+++ config.py
@@ -6,10 +6,11 @@ PREFIX = "g"
 # Game Settings
 MAX_WISHES = 10
 RESET_TIME = datetime.timedelta(minutes=20)
-DB_PATH = "data/gacha.db"
+DB_PATH = "casino.db"
 
 # Discord Settings
 OWNER_ID = 873464016217968640
 
 # Maintenance Mode
+
 MAINTENANCE_MODE = False
